f4ceea5194366679bf1c2ab0a113bec5da34cb73
cgonel <gonelw@gmail.com>
2021-11-30 14:15:11 -0500

    set candidates & votes

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,6 +2,33 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Voting {
+  mapping(int256 => string) public candidates;
+  mapping(uint256 => uint256) public votes;
+  mapping(address => uint256) public hasVoted;
+
+  // event VoteMade(uint _for, address _by);
+
   constructor() public {
+    // set the 2 candidates - Michael Scott, Dwight Schrute
+    candidates[1] = "Michael Scott";
+    candidates[2] = "Dwight Schrute";
+    votes[1] = 0;
+    votes[2] = 0;
   }
+
+  function vote(uint256 _candidateId) public {
+    require(hasVoted[msg.sender] == 0);
+
+    // votes[_candidateId]++;
+    // hasVoted[msg.sender] = 1;
+
+    // emit VoteMade(_candidateId, msg.sender);
+  }
+
 }
+
+// there is 2 candidates OK
+// both candidates start at 0 votes OK
+// each address can only vote once
+// need to keep track of amount of votes for each candidate
+// have an event to trigger when someone votes
--- test/voting.js
+++ test/voting.js
@@ -5,9 +5,49 @@ const voting = artifacts.require("voting");
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("voting", function (/* accounts */) {
+contract("voting", function (accounts) {
+  var accHasntVoted = accounts[1];
+  var accHasVoted = accounts[2];
+
+
   it("should assert true", async function () {
     await voting.deployed();
     return assert.isTrue(true);
+  });
+
+  it("sets the candidates of the vote", function(){
+    return voting.deployed().then((instance) => {
+      votingInstance = instance;
+      return votingInstance.candidates(1);
+    }).then((name) => {
+      assert.equal(name, "Michael Scott", 'name of first candidate is correct');
+      return votingInstance.candidates(2);
+    }).then((name) => {
+      assert.equal(name, "Dwight Schrute", 'name of the second candidate is correct');
+    })
+  });
+
+  it("both candidates should start with no votes", function(){
+    return voting.deployed().then((instance) => {
+      votingInstance = instance;
+      return votingInstance.votes(1);
+    }).then((number) => {
+      assert.equal(number, 0, 'votes for first candidate starts at 0');
+      return votingInstance.votes(2);
+    }).then((number) => {
+      assert.equal(number, 0, 'votes for the second candidate starts at 0');
+    })
+  });
+
+  it('can not vote if already has voted', function() {
+    return voting.deployed().then((instance) => {
+      votingInstance = instance;
+      votingInstance.hasVoted[accHasVoted] = 1;
+      votingInstance.hasVoted[accHasntVoted] = 0;
+    
+      return votingInstance.vote(1, { from: accHasVoted });
+    }).then((receipt) => {
+
+    })
   });
 });
